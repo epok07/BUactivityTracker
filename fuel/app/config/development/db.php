ff9165ed8605aa2ff417a4c347dde01187895397
Franck Elombat <f.elombat@gmail.com>
2016-10-14 09:29:11 +0100

    Set Dev Environement data info

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
-			'username'   => 'root',
-			'password'   => 'root',
+			'dsn'        => 'mysql:host=localhost;dbname=sodisnidb',
+			'username'   => 'sodisni',
+			'password'   => 'Eur3k@',
 		),
 	),
 );
